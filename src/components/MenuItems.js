4d10162d6684714f664d0e2684361058f5ada942
vari <bryan_99yong@hotmail.com>
2024-01-27 14:18:12 -0500

    First commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title : "Home",
+        url :  "/",
+        cName: "nav-links",
+        icon : "fa-solid fa-house-chimney"
+    },
+    {
+        title : "About",
+        url :  "/about",
+        cName: "nav-links",
+        icon : "fa-solid fa-circle-info"
+    },
+    {
+        title : "Service",
+        url :  "/service",
+        cName: "nav-links",
+        icon : "fa-solid fa-briefcase"
+    },
+    {
+        title : "Contac us",
+        url :  "/contacus",
+        cName: "nav-links",
+        icon : "fas fa-address-book"
+    },
+    {
+        title : "Sign Up",
+        url :  "/singup",
+        cName: "nav-links-mobile",
+    },
+
+]
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+
+class Navbar extends Component {
+    state = {clicked: false};
+    handleClick = () =>{
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <img src={logo} alt="Logo" className="Logo" />
+                <h1 className="navbar-logo">Amazon Natural Healing Center</h1>
+
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href="/">
+                                    <i className={item.icon}></i>
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })}
+                    <button>Sing up</button>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
