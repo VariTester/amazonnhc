4cc555301f55f95ddd5fe64ad6d943ff1175430c
vari <bryan_99yong@hotmail.com>
2024-01-29 18:48:27 -0500

    avanzandoo

--- src/routes/Ayahuasca.js
+++ src/routes/Ayahuasca.js
@@ -1,7 +1,22 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+import AyahuascaImage from "../assets/images/ayahuasca.png"
+
 function Ayahuasca (){
     return(
         <>
-        <h1>This is Ayahuuasca Retreats</h1>
+        <Navbar/>
+        <Hero 
+            cName = "hero"
+            // heroImg = "https://images.unsplash.com/photo-1570998401604-dc1cb6929fff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            heroImg = {AyahuascaImage}
+            title = "Ready for the amazing experience?"
+            text = "Choose Your Favourite Retreat Plan"
+            // buttonText = "Retreat Plans"
+            // url = "/"
+            // btnClass = "show"
+        />
         </>
     )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,18 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+
+import HomeImage from "../assets/images/river.png"
 function Home (){
     return(
         <>
         <Navbar/>
         <Hero 
             cName = "hero"
-            heroImg = "https://images.unsplash.com/photo-1610413341456-e283a1c6026f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            title = "Your Journey Your Story"
-            text = "Choose Your Favourite Destination"
-            buttonText = "Travel Plan"
+            // heroImg = "https://images.unsplash.com/photo-1610413341456-e283a1c6026f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            heroImg = {HomeImage}
+            title = "Transform your life, find peace"
+            text = "Choose Your Favourite Retreat"
+            buttonText = "Retreat Plans"
             url = "/"
             btnClass = "show"
         />
