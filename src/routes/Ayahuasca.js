d24c8e89d1b5ee389ca4c16988d134706aa1ecb3
vari <bryan_99yong@hotmail.com>
2024-01-29 19:17:48 -0500

    avance

--- src/routes/Ayahuasca.js
+++ src/routes/Ayahuasca.js
@@ -1,14 +1,14 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
-import AyahuascaImage from "../assets/images/ayahuasca.png"
+import AyahuascaImage from "../assets/images/ayahuascarecortado.png"
 
 function Ayahuasca (){
     return(
         <>
         <Navbar/>
         <Hero 
-            cName = "hero"
+            cName = "hero-mid"
             // heroImg = "https://images.unsplash.com/photo-1570998401604-dc1cb6929fff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             heroImg = {AyahuascaImage}
             title = "Ready for the amazing experience?"
