585275fa6c3c78ffba9d713464bbc3c9264650fc
vari <bryan_99yong@hotmail.com>
2024-01-30 19:13:20 -0500

    porseguridad

---
+++ src/components/DestinationData.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import "./FirstPartHomeStyless.css"
+
+import HomeImg1 from "../assets/images/shipibo.png"
+import HomeImg2 from "../assets/images/shaman.png"
+import HomeImg3 from "../assets/images/logo.png"
+import HomeImg4 from "../assets/images/logo.png"
+
+class DestinationData extends Component{
+    render(){
+        return(
+            <div className={this.props.classname}>
+            <div className="des-text">
+                <h2>{this.props.heading}</h2>
+                <p>
+                 {this.props.text1}
+                 <br/>
+                 <br/>
+                </p>
+                <p>
+                {this.props.text2}
+                </p>
+            </div>
+            <div className="image">
+                <img alt="img" src={this.props.img1}/>
+                <img alt="img" src={this.props.img2}/>
+            </div>
+        </div>
+        )
+    } 
+}
+
+export default DestinationData
