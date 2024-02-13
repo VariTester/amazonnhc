d1d33b42126c4efe5ad6961a07327165393db19d
vari <bryan_99yong@hotmail.com>
2024-02-13 12:23:11 -0500

    sinvalidacion

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,17 +1,52 @@
-import "./ContactFormStyles.css"
+import "./ContactFormStyles.css";
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
+// import { useForm } from "react-hook-form";
 
 function ContactForm(){
+
+    // const {register, handleSubmit} = useForm();
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_sscuyur', 'template_567amfp', form.current, {
+        publicKey: 'RDaj7IbvCZ2gAJhjL',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+  
     return(
         <div className="form-container">
             <h1>Send a message to us!</h1>
-            <form>
-                <input placeholder="Name"/>
-                <input placeholder="Email"/>
-                <input placeholder="Phone Number"/>
-                <textarea placeholder="Message" rows="4"/>
+            <form ref={form} onSubmit={sendEmail}>
+                <input placeholder="Name" type="text" name="user_name"/>
+                <input placeholder="Email" type="email" name="user_email"/>
+                <input placeholder="Phone Number" type="number" name="user_phone"/>
+                <input placeholder="Country" type="text" name="user_country"/>
+                <textarea placeholder="Message" name="message" rows="4"/>
                 <button>Send Message</button>
             </form>
         </div>
+
+    //     <form ref={form} onSubmit={sendEmail}>
+    //     <label>Name</label>
+    //     <input type="text" name="user_name" />
+    //     <label>Email</label>
+    //     <input type="email" name="user_email" />
+    //     <label>Message</label>
+    //     <textarea name="message" />
+    //     <input type="submit" value="Send" />
+    //   </form>
     )
 }
 
