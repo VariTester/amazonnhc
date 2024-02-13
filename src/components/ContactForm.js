c9cd33f2fbe957e7aa76b1ca401f3a4c541d154f
vari <bryan_99yong@hotmail.com>
2024-02-13 17:19:30 -0500

    termiinandocontactus

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,53 +1,81 @@
 import "./ContactFormStyles.css";
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
-// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-function ContactForm(){
-
-    // const {register, handleSubmit} = useForm();
+function ContactForm() {
+    const {register, formState:{errors}, handleSubmit, reset} = useForm();
+    const [isSuccess, setIsSuccess] = useState(false);
     const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const sendEmail = (data) => {
+        if (Object.keys(errors).length === 0) {
+            emailjs
+                .sendForm('service_sscuyur', 'template_567amfp', form.current, {
+                    publicKey: 'RDaj7IbvCZ2gAJhjL',
+                })
+                .then(
+                    () => {
+                        setIsSuccess(true);
+                        setTimeout(() => {
+                            setIsSuccess(false);
+                        }, 3000);
+                    },
+                    (error) => {
+                        console.log('FAILED...', error.text);
+                    },
+                );
+            reset();
+        }
+    }
 
-    emailjs
-      .sendForm('service_sscuyur', 'template_567amfp', form.current, {
-        publicKey: 'RDaj7IbvCZ2gAJhjL',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
-  
     return(
         <div className="form-container">
             <h1>Send a message to us!</h1>
-            <form ref={form} onSubmit={sendEmail}>
-                <input placeholder="Name" type="text" name="user_name"/>
-                <input placeholder="Email" type="email" name="user_email"/>
-                <input placeholder="Phone Number" type="number" name="user_phone"/>
-                <input placeholder="Country" type="text" name="user_country"/>
-                <textarea placeholder="Message" name="message" rows="4"/>
-                <button>Send Message</button>
+            <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+                <input placeholder="Name" type="text" {...register('user_name', {
+                    required: true,
+                    maxLength: 20,
+                })}/>
+                {errors.user_name?.type === 'required' && <p>El Nombre es obligatorio</p>}
+                {errors.user_name?.type === 'maxLength' && <p>Solo puede ingresar 20 caracteres</p>}
+                
+                <input placeholder="Email" type="email" {...register('user_email',{
+                    required: true,
+                    maxLength: 30,
+                    pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/
+                })}/>
+                {errors.user_email?.type === 'required' && <p>El Email es obligatorio</p>}
+                {errors.user_email?.type === 'maxLength' && <p>No puede ingresar más palabras</p>}
+                {errors.user_email?.type === 'pattern' && <p>El formato del email es incorrecto</p>}
+                
+                <input placeholder="Phone Number" type="number" {...register('user_phone',{
+                    required: true,
+                    maxLength: 15,
+                    minLength: 7,
+                })}/>
+                {errors.user_phone?.type === 'required' && <p>Ingrese un número de celular o teléfono válido</p>}
+                {errors.user_phone?.type === 'minLength' && <p>Debe ingresar más números</p>}
+                {errors.user_phone?.type === 'maxLength' && <p>Debe ingresar menos números</p>}
+                
+                <input placeholder="Country" type="text" {...register('user_country',{
+                    required: true,
+                    maxLength: 50,
+                })}/>
+                {errors.user_country?.type === 'required' && <p>El País es obligatorio</p>}
+                
+                <textarea placeholder="Message" {...register('message',{
+                    required: true,
+                    minLength: 10,
+                })} rows="4"/>
+                {errors.user_message?.type === 'required' && <p>Debe brindarnos más información</p>}
+                {errors.user_message?.type === 'minLength' && <p>Cuentenos un poco más sobre lo que desea</p>}
+                
+                <button type="submit">Send Message</button>
             </form>
+            {isSuccess && <p>Mensaje enviado con éxito</p>}
         </div>
-
-    //     <form ref={form} onSubmit={sendEmail}>
-    //     <label>Name</label>
-    //     <input type="text" name="user_name" />
-    //     <label>Email</label>
-    //     <input type="email" name="user_email" />
-    //     <label>Message</label>
-    //     <textarea name="message" />
-    //     <input type="submit" value="Send" />
-    //   </form>
-    )
+    );
 }
 
-export default ContactForm
+export default ContactForm;
